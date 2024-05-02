245c60cebbf11c6c397826a25f50ce3f339c7de8
VioletaHoza <hoza.il.violeta@student.utcluj.ro>
2024-05-02 23:18:00 +0300

    update

--- src/main/java/Presentation/OperationController.java
+++ src/main/java/Presentation/OperationController.java
@@ -54,5 +54,4 @@ public class OperationController<T> extends JFrame {
                 break;
         }
     }
-
 }
--- src/main/java/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"BusinessLogic","l":"ClientBLL"},{"p":"BusinessLogic","l":"OrderBLL"},{"p":"BusinessLogic","l":"ProductBLL"}];updateSearchResults();
